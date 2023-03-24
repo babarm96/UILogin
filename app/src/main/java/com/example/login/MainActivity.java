d2b371c04fadda7d99471548497b4b5c62eaa451
admin <sejalzarekar1530@gmail.com>
2023-03-24 20:31:40 +0530

    commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.login;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler= new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(MainActivity.this, login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
+
+    }
+}
---
+++ app/src/main/java/com/example/login/login.java
@@ -0,0 +1,18 @@
+package com.example.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
